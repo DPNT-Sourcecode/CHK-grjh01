484ac5b22b64c0fc21c4f4128d058a605e940fc1
sbx_user1051 <sbx_user1051@169.254.16.5>
2023-08-07 21:38:40 +0000

    Mon Aug 07 21:38:40 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,10 @@
-from solutions.SUM import sum_solution
+from accelerate_runner.lib.solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_invalid_sum(self):
+        assert sum_solution.compute("1", 2) == 3
+
